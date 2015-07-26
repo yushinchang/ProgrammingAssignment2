b13466a131e121ef112d1fec2aca2b739d36c5b8
yushin <yushinchang0@gmail.com>
2015-07-26 16:35:16 +0800

    failed ground test

--- cachematrix.R
+++ cachematrix.R
@@ -2,36 +2,29 @@
 ## This R file is aimed at caching inverse of a matrix
 
 ## Write a short comment describing this function
-
+m <- matrix(c(2,1,5,3),2,2)
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
-    x <<- y
-    i <<- NULL
+  cached_inverse <- NULL
+  setinverse <- function(inverse) {
+    cached_inverse <<- inverse
   }
-  get <- function() x
-  setinverse <- function(inverse){
-    i <<- inverse
-  }
-  getinverse <- function(){
-    i
-  }
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+  list(self=x,cached_inverse=cached_inverse,setinverse=setinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  i <- x$getinverse()
+  i <- x$cached_inverse
   if(!is.null(i)) {
     message("getting cached data")
     return(i)
   }
-  data <- x$get()
+  data <- x$self
   i <- solve(data, ...)
   x$setinverse(i)
+  print('x$cached')
+  print(x$cached_inverse)
+  print('i:')
   i
 }
